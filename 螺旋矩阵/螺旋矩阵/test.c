5f7c00f9dd4f8543657407309516664d8e958c50
863977171 <863977171@qq.com>
2021-04-11 21:52:41 +0800

    。。。

---
+++ 螺旋矩阵/螺旋矩阵/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int* spiralOrder(int** matrix, int matrixRowLen, int* matrixColLen, int* returnSize) {
+	
+}
+
+int main()
+{
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	spira10rder(arr[0][0],3,3,9);
+	return 0;
+}
